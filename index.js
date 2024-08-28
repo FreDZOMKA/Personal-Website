0076f2b8ac9678435f485d3f27dcc442a193c7a6
017-Fredrik <107189080+FreDZOMKA@users.noreply.github.com>
2024-08-28 11:20:53 +0200

    Nav update, added square

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //Elements
 const timeElement = document.getElementById("timeEl");
-const devDesElement = document.getElementById("intro-text");
+const devDesElement = document.getElementById("intro-head");
+const faceSquareElement = document.getElementById("faceSquare")
 
 //Assignments
-const introtext = "Design. Develop."
+const introhead = "Design. Develop."
 let index = 0;
 
 const interval = setInterval(() => {
-  if (index < introtext.length){
-    devDesElement.textContent += introtext[index];
+  if (index < introhead.length){
+    devDesElement.textContent += introhead[index];
     index++;
   } else {
     clearInterval(interval)
@@ -34,13 +35,20 @@ function updateTime() {
 }
 updateTime();
 
+//Increase Opacity of Text element
 let opacityInit = 0;
 if (opacityInit == 0) {
   setInterval(() => {
     if (opacityInit < 1) {
       opacityInit += 0.03;
-      console.log(opacityInit);
+      console.log("Opacity is being Increased");
       devDesElement.style.opacity = opacityInit;
     }
   }, 30);
 }
+//Dragable Square
+  faceSquareElement.addEventListener("click",()=>{
+    console.log("hi square")
+    faceSquareElement.style.backgroundColor = "blue"
+  })
+
