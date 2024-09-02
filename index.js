b8a3823c9cdc8106dad707884c4d0b42a292362f
017-Fredrik <107189080+FreDZOMKA@users.noreply.github.com>
2024-09-02 13:02:28 +0200

    Styling, Added Starfield
    Added stars section that generates based on time of day.
    
    Fixed CV link
    Added projects section with SVG arrow used for toggling (function not added).

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 //Elements
 const timeElement = document.getElementById("timeEl");
 const devDesElement = document.getElementById("intro-head");
-const faceSquareElement = document.getElementById("faceSquare")
+const introTextElement = document.getElementById("intro-text");
+const faceSquareElement = document.getElementById("faceSquare");
+const starFieldElement = document.getElementById("starField");
+
+starFieldElement.addEventListener("click", () => {
+  console.log("click");
+});
+
+const now = new Date();
+const hours = now.getHours();
+const minutes = now.getMinutes();
 
 //Assignments
-const introhead = "Design. Develop."
+const introhead = "Design. Develop.";
+const introText = "Fredrik, a programmer and graphic designer based in southern Sweden.";
 let index = 0;
+let index1 = 0;
 
-const interval = setInterval(() => {
-  if (index < introhead.length){
+//Text typing
+const introHeadInterval = setInterval(() => {
+  if (index < introhead.length) {
     devDesElement.textContent += introhead[index];
     index++;
   } else {
-    clearInterval(interval)
+    clearInterval(introHeadInterval);
+    writeText();
   }
-  
-}, 100);
+}, 90);
+function writeText() {
+  const introTextInterval = setInterval(() => {
+    if (index1 < introText.length) {
+      introTextElement.textContent += introText[index1];
+      index1++;
+    } else {
+      clearInterval(introTextInterval);
+    }
+  }, 40);
+}
 
 //Functions
 function updateTime() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-
   // Format the string with leading zeroes
   const clockStr = `${hours.toString().padStart(2, "0")}:${minutes
     .toString()
@@ -46,9 +65,25 @@ if (opacityInit == 0) {
     }
   }, 30);
 }
-//Dragable Square
-  faceSquareElement.addEventListener("click",()=>{
-    console.log("hi square")
-    faceSquareElement.style.backgroundColor = "blue"
-  })
 
+//Generate Starfield
+function starGeneration() {
+  const starEl = document.createElement("img");
+  starEl.innerText = "JSTAR";
+  starEl.setAttribute("src", "/External-Resources/flowers.svg");
+  starEl.classList.add("smallStars");
+
+  starFieldElement.appendChild(starEl);
+
+  console.log("starGeneration function was run");
+}
+
+let amountOfStars = 0;
+const printStar = setInterval(() => {
+  if (amountOfStars < hours) {
+    starGeneration();
+    amountOfStars++;
+  } else {
+    clearInterval(printStar);
+  }
+}, 1);
